bc6b1e0c15cc657f20191f26ac7b2b2fc58bacfd
Selien Xavier Yorbandi <selienyorbandi@gmail.com>
2022-02-28 18:54:15 -0300

    Footer comp. was added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import NavBar from "./components/NavBar/NavBar";
 
@@ -11,6 +12,7 @@ function App() {
       <main>
         <ItemListContainer/>
       </main>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,36 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  faFacebookF, faInstagram, faWhatsapp, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import NewsletterAdd from "./NewsletterAdd/NewsletterAdd";
+import styles from "./styles.module.css";
+
+function Footer(props) {
+  return (
+    <>
+      <NewsletterAdd />
+      <footer className={styles.Footer}>
+        <div className={styles.Footer__content}>
+          <ul className={styles.Footer__content__row}>
+            <li>Botón de arrepentimiento</li>
+            <li>Términos y condiciones</li>
+            <li>Política de privacidad</li>
+            <li>Garantía y devoluciones</li>
+            <li>Preguntas frecuentes</li>
+          </ul>
+          <hr className={styles.Footer__content__hr}/>
+          <ul className={styles.Footer__content__row}>
+                <li>Copyright © {new Date().getFullYear()} Computahorra</li>
+                <li>Desarrollado por Selien Yorbandi</li>
+          </ul>
+          <ul className={styles.Footer__content__row}> 
+                <li><FontAwesomeIcon icon={faFacebookF} size="2xl"/></li>
+                <li><FontAwesomeIcon icon={faInstagram} size="2xl"/></li>
+                <li><FontAwesomeIcon icon={faWhatsapp} size="2xl"/></li>
+                <li><FontAwesomeIcon icon={faYoutube} size="2xl"/></li>
+          </ul>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
