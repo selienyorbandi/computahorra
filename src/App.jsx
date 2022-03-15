bd7cf37c1d9a90c74fe4b1c33994f859cb7403a7
Selien Yorbandi <selienxavieryorbandi@gmail.com>
2022-03-15 19:22:42 -0300

    Challenge 8 - Events

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "components/Footer/Footer";
 import ItemListContainer from "Containers/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "Containers/ItemDetailContainer/ItemDetailContainer";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Cart from "components/Cart/Cart";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<ItemListContainer categoryFilter={false}/>}/>
             <Route path="/item/:id" element={<ItemDetailContainer />}/>
             <Route path="/category/:id" element={<ItemListContainer categoryFilter={true}/>}/>
+            <Route path="/cart" element={<Cart/>}/>
             <Route path="*" element={<Navigate replace to="/"/>}/>
           </Routes>
         </main>
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,37 @@
+import styles from "./styles.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleUp, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import Button from "components/Button/Button";
+
+function ItemCount({stock, initial, onAdd, message, size, primary, expanded}){
+  const [amount, setAmount] = useState(initial);
+
+  function increaseAmount(){
+    if(amount < stock) {
+      setAmount(amount+1);
+    }
+  }
+
+  function decreaseAmount(){
+    if(amount > 1) {
+      setAmount(amount-1);
+    }
+  }
+    
+  return(
+    <div className={styles.ItemCount}>
+      <Button message={message} onClick={onAdd} primary={primary} size={size}/>
+      {expanded ? stock > 0 ? <div className={styles.ItemCount__stock}>¡Stock disponible!</div> : <div className={`${styles.ItemCount__stock} ${styles.ItemCount__stock_red}`}>Sin stock</div> : <></>}
+      <div className={styles.ItemCount__count}>
+        <span className={styles.ItemCount__display}>{amount}</span>
+        <div className={styles.ItemCount__controllers}>
+          <span onClick={increaseAmount}><FontAwesomeIcon icon={faAngleUp} size="xs"/></span><span onClick={decreaseAmount}><FontAwesomeIcon icon={faAngleDown} size="xs"/></span>
+        </div>
+      </div>
+            
+    </div>
+  );
+}
+
+export default ItemCount;
