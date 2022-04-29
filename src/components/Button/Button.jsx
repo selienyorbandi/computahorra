8f2daa2621e04e91479ec27b028174a117958244
Selien Yorbandi <selienxavieryorbandi@gmail.com>
2022-04-29 03:31:21 -0300

    Btn extra colors

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,23 @@
 import styles from "./styles.module.css";
 
-function Button({message, onClick, primary, size, disabl=false}) {
+function Button({message, onClick, primary, size, disabl=false, color}) {
     
+  const priCol = `Button__primary__${color}`;
+  const secCol = `Button__secondary__${color}`;
+
   return (
-    <button className={`${styles.Button} ${primary ? styles.Button__primary : styles.Button__secondary} ${size === "sm" ? styles.Button__sm : styles.Button__md}`} onClick={onClick} disabled={disabl}>{message}</button>
+    <button 
+      className={
+        `
+        ${styles.Button} 
+        ${primary ? styles.Button__primary : styles.Button__secondary} 
+        ${size === "sm" ? styles.Button__sm : styles.Button__md}
+        ${primary ? color && styles[priCol] : color && styles[secCol]}
+        `
+      } 
+      onClick={onClick} 
+      disabled={disabl}>{message}
+    </button>
   );
 }
 
