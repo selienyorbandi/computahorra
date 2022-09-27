57571e04fd44d26064bd18531875a11c8d5e224b
Selien Yorbandi <selienyorbandi@gmail.com>
2022-09-27 15:59:31 -0300

    Actualización a react 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import getFirestoreApp from "./firebase/config.js";
 
 import App from "./App";
@@ -8,9 +8,9 @@ import "index.css";
 
 getFirestoreApp();
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
